6b9504ab1303ec93e2c2619b5b8bbce542f3585f
Nikolas Tapia <tapia@wias-berlin.de>
2022-06-09 13:46:40 +0200

    added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,43 @@
 #[cfg(test)]
+
 mod tests {
-    use super::pvar::p_var_backbone;
+    use super::pvar::*;
+    use rand::prelude::*;
+    use rand::distributions::Standard;
+
+    fn dist<const N: usize>(a: [f64; N], b: [f64; N]) -> f64 {
+        f64::sqrt(a.iter().zip(b.iter()).map(|(&x, &y)| (x - y).powf(2.)).sum())
+    }
+
+    fn dist_1d(a: f64, b: f64) -> f64 {
+        f64::abs(a - b)
+    }
+
     #[test]
-    fn backbone() {
-        let v = (0..=10).map(f64::from).collect::<Vec<f64>>();
-        assert_eq!(p_var_backbone(&v, 1., |a, b| f64::abs(a - b)), Ok(10.));
+    fn square_path() {
+        let v: [[f64; 2]; 8] = [[0., 0.], [0., 1.], [1., 1.], [1., 0.], [0., 0.], [0., 0.], [0., 0.], [0., 0.]];
+        let mut p = 1.;
+        while p <= 4. {
+            assert_eq!(p_var_backbone(&v, p, dist), p_var_backbone_ref(&v, p, dist));
+            p += 0.5;
+        }
+    }
+
+    #[test]
+    fn bm() {
+        const N: usize = 2500;
+        let mut path = [0.; N + 1];
+        let sigma = 1. / f64::sqrt(N as f64);
+        path[1..].copy_from_slice(&StdRng::from_entropy().sample::<[bool; N], Standard>(Standard).map(|x| if x { sigma } else { -sigma }).iter().scan(0., |acc, x| { *acc += x; Some(*acc) }).collect::<Vec<_>>());
+        for p in [1., f64::sqrt(2.), 2., f64::exp(1.)] {
+            assert_eq!(p_var_backbone(&path, p, dist_1d), p_var_backbone_ref(&path, p, dist_1d));
+        }
+    }
+
+    #[test]
+    fn errors() {
+        assert_eq!(format!("{}", p_var_backbone(&[], 1., dist_1d).err().unwrap()), "input array is empty");
+        assert_eq!(format!("{}", p_var_backbone(&[0., 1.], 0., dist_1d).err().unwrap()), "exponent must be greater or equal than 1.0");
     }
 }
 
